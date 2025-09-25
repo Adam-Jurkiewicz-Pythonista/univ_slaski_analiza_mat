1f1cda5022d3cca53380ee112a80c466b55cea66
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-09-25 11:49:28 +0200

    test

--- run_one.py
+++ run_one.py
@@ -2,6 +2,7 @@ from analiza_fn.obrazy import Obraz, KMeansObraz
 
 q = Obraz('100MA_300g_2_06.tif')
 print(q)
+print("inny")
 p = Obraz('Fe1i5Cr1i5_obszar2_x1000_x12_y5_R1_C1.jpg')
 print(p)
 r = Obraz('trump.jpg')
